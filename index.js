cb807ed3334512c568561027e9b2818b7e15aaa1
michaeli-g <michaelianguerrero@gmail.com>
2022-10-27 07:28:56 +0000

    https inc

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
-const app = express()
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
 const port = 8080
+const sport = 443
 
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+
+const app = express()
 
 app.use(express.static(__dirname));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
@@ -13,6 +24,10 @@ app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/assets/index.html')
 })
 
-app.listen(port, () => {
-	console.log('Michaels listening on ' + port)
-})
+// app.listen(port, () => {
+// 	console.log('Michaels listening on ' + port)
+// })
+
+
+//http.createServer(app).listen(port)
+https.createServer(options, app).listen(sport)
