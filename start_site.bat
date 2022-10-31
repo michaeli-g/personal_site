29f3aeee3b5e4e89612654a83b8b23ef1cd0a264
Ubuntu <ubuntu@ip-172-31-32-253.us-east-2.compute.internal>
2022-10-31 21:28:33 +0000

    removed start scripts

--- start_site.bat
+++
@@ -1,3 +0,0 @@
-START /B node assets/templates/smart-ecu/index.js 
-START /B node assets/templates/launchpad-site/index.js
-node index.js
--- start_site.sh
+++
@@ -1,3 +0,0 @@
-node assets/templates/smart-ecu/index.js
-node assets/templates/launchpad-site/index.js
-node index.js
