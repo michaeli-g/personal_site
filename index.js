938c905a8473fb3fa48d99f230794484d1b8a024
michaeli-g <michaelianguerrero@gmail.com>
2022-10-31 23:04:18 -0500

    added sites

--- index.js
+++ index.js
@@ -6,31 +6,65 @@ const port = 8080
 const sport = 443
 
 
-const options = {
+/*const options = {
   key: fs.readFileSync('../privkey.pem'),
   cert: fs.readFileSync('../fullchain.pem')
-};
+};*/
 
 
 const app = express()
+
 
 app.use(express.static(__dirname));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
 
-app.enable('trust proxy')
+
+//Redirects to https
+/*app.enable('trust proxy')
 app.use((req, res, next) => {
 	    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
-})
+})*/
+
 
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/assets/index.html')
 })
 
-// app.listen(port, () => {
-// 	console.log('Michaels listening on ' + port)
-// })
+//Routing for launchpad
+app.get('/launchpad', (req, res) => {
+	res.sendFile(__dirname + '/assets/templates/launchpad-site/assets/index.html')
+})
 
 
-http.createServer(app).listen(80)
-https.createServer(options, app).listen(sport)
+app.get('/launchpad', (req, res) => {
+	res.sendFile(__dirname + '/assets/templates/launchpad-site/assets/index.html')
+})
+
+
+app.get('/secu', (req, res) => {
+	res.sendFile(__dirname + '/assets/templates/smart-ecu/assets/index.html')
+})
+
+app.get('/secu-team', (req, res) => {
+	res.sendFile(__dirname + '/assets/templates/smart-ecu/assets/team.html')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+ app.listen(port, () => {
+ 	console.log('Michaels listening on ' + port)
+ })
+
+
+/*http.createServer(app).listen(80)
+https.createServer(options, app).listen(sport)*/
